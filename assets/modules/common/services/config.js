cd9f4971691ab64785489b1240eba3d5e8695fa7
Luis Cruz <cruz.torrez@gmail.com>
2015-06-22 15:11:23 -0500

    new notifications for asociados

--- assets/modules/common/services/config.js
+++ assets/modules/common/services/config.js
@@ -1,18 +1,18 @@
 
 ///Desarrollo
 
-angular.module('app.common.services').constant('Config', {
-    // REST: 'http://localhost:8000',
-    // REST: 'http://54.187.171.36',
-    REST: 'http://localhost:8000',
-    STATIC: 'http://localhost:4000/',
-});
+// angular.module('app.common.services').constant('Config', {
+//     // REST: 'http://localhost:8000',
+//     // REST: 'http://54.187.171.36',
+//     REST: 'http://localhost:8000',
+//     STATIC: 'http://localhost:4000/',
+// });
 
 
 //Production//
 
-// angular.module('app.common.services').constant('Config', {
-//     // REST: 'http://localhost:8000',
-//     REST: 'http://52.11.113.25',
-//     STATIC: 'https://s3-us-west-2.amazonaws.com/asociados-client/public/',
-// });
+angular.module('app.common.services').constant('Config', {
+    // REST: 'http://localhost:8000',
+    REST: 'http://52.11.113.25',
+    STATIC: 'https://s3-us-west-2.amazonaws.com/asociados-client/public/',
+});
