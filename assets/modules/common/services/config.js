c7184b885814496d0e1036d8c4c5b35aee34b8d1
Camilo Ramirez <camilortte@hotmail.com>
2015-10-01 08:59:25 -0500

    Visual changes for notification and pagination

--- assets/modules/common/services/config.js
+++ assets/modules/common/services/config.js
@@ -1,7 +1,8 @@
 
 if(ENV == 'Dev'){
     angular.module('app.common.services').constant('Config', {
-        REST: 'http://localhost:8000',
+        // REST: 'http://localhost:8000',
+        REST: 'http://52.11.113.25',
         STATIC: 'http://localhost:4000',
     });
 }
