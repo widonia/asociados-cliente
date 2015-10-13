a4e16deeed277a8ec250f68b9cb84d8a11365a57
Luis Cruz <cruz.torrez@gmail.com>
2015-10-13 11:56:30 -0500

    crud init

---
+++ assets/modules/common/controllers/list.js
@@ -0,0 +1,49 @@
+"use strict";
+
+function CommonListCtrl($rootScope, CRUDService){
+
+    this.count = 0;
+    this.page = 1;
+    this.list = [];
+
+    this.init = function(){
+        this.getList();
+    }
+
+    this.setPage = function () {
+        this.getList();
+    };
+
+    this.getList = function(){
+        $rootScope.$broadcast('loading-show');
+        CRUDService.get({page:this.page}, this.onGetList.bind(this));
+    }
+
+    this.onGetList = function(response){
+        $rootScope.$broadcast('loading-hide');
+
+        this.count = response.count;
+        this.list = response.results;
+        window.scrollTo(0, 0);
+    }
+
+    this.delete = function(id){
+        event.preventDefault();
+        var confirmDelete = confirm('Esta seguro de querer borrar este elemento?');
+
+        if (confirmDelete) {
+            $rootScope.$broadcast('loading-show');
+            CRUDService.delete({id:id}, this.onDelete.bind(this));
+        }
+    }
+
+    this.onDelete = function(){
+        this.getList();
+    }
+
+    this.init();
+}
+
+angular
+    .module('app.notification.controllers')
+    .controller('CommonListCtrl', CommonListCtrl);
