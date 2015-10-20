924ec5ba634e6c989b54803e02b8fb3b4e442405
Camilo Ramirez <camilortte@hotmail.com>
2015-10-20 12:28:42 -0500

    Fix erro module common

--- assets/modules/common/module.js
+++ assets/modules/common/module.js
@@ -2,10 +2,12 @@
 angular.module('app.common.services', []);
 angular.module('app.common.directives', []);
 angular.module('app.common.filters', []);
+angular.module('app.common.controllers', []);
 
 angular.module('app.common', [
     'app.common.services',
     'app.common.directives',
     'app.common.filters',
+    'app.common.controllers',
 ]);
 
