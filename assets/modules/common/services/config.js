6788e049154dadbf81ad2e343825e8e066949a15
Luis Cruz <cruz.torrez@gmail.com>
2015-10-26 12:23:37 -0500

    search by document

--- assets/modules/common/services/config.js
+++ assets/modules/common/services/config.js
@@ -1,7 +1,7 @@
 if(ENV == 'Dev'){
     angular.module('app.common.services').constant('Config', {
-        // REST: 'http://localhost:8000',
-        REST: 'http://52.11.113.25',
+        REST: 'http://localhost:8000',
+        // REST: 'http://52.11.113.25',
         STATIC: 'http://localhost:4000',
         MEDIA: 'http://localhost:8000/media',
     });
