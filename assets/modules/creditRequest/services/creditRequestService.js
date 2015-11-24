c4339ba9b9c058326677f3947cf0aa86348d5fdc
Camilo Ramirez <camilortte@hotmail.com>
2015-11-24 09:16:19 -0500

    Fix changes to deplogy in gulp

--- assets/modules/creditRequest/services/creditRequestService.js
+++ assets/modules/creditRequest/services/creditRequestService.js
@@ -8,7 +8,7 @@ function CreditRequestService($resource, Config){
         get: { method:'GET'},
         proccess: { method:'POST', url:_url + 'processed' , params: {id: "@id"}},
         put: { method:'PUT', url:_url , params: {id: "@id"}},
-        download_data: { method:'GET', url: Config.REST + '/api/statements/download/'}, 
+        download_data: { method:'GET', url: Config.REST + '/api/credit_request/download/'},         
         // post: { method:'POST'},
         // put: { method:'PUT'},
         // delete: { method:'DELETE'},
