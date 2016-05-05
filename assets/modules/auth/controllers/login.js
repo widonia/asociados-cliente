16a3c3eb471e12766237ffd629bf4f3194c48afc
Camilo Ramirez <camilortte@hotmail.com>
2016-05-05 12:56:23 -0500

    Fix errpr desactivate coop

--- assets/modules/auth/controllers/login.js
+++ assets/modules/auth/controllers/login.js
@@ -1,6 +1,6 @@
 "use strict";
 
-function LoginCtrl($rootScope, $routeParams, $location, Config, AuthService, AuthManager, UserService, AUTH_EVENTS){
+function LoginCtrl($rootScope, $routeParams, $location, Config, AuthService, AuthManager, UserService, AUTH_EVENTS, SweetAlert){
 
     this.form = false;
     this.error = false;
@@ -55,7 +55,12 @@ function LoginCtrl($rootScope, $routeParams, $location, Config, AuthService, Aut
     }
 
     this.onCoopListOk = function(response, username, last_login){
-        this.selectCoop(response.data[0].cooperative.id, response.data[0].role, username, last_login);
+        if(response.data.length <= 0){
+            SweetAlert.swal("Error", "Este usuario no tiene acceso a esta cooperativa o la cooperativa esta desactivada.", "error");
+        }else{
+             this.selectCoop(response.data[0].cooperative.id, response.data[0].role, username, last_login);
+        }
+        
         // var size = Object.keys(response.data).length;
 
         // if(size == 1){
