8fba336d77bd1a151ae1e496dddb1ab73319a189
Jonathan Prieto <jonathan@enlazar.net>
2017-09-25 14:46:27 -0500

    version 2

--- assets/modules/common/services/config.js
+++ assets/modules/common/services/config.js
@@ -1,21 +1,20 @@
 if(ENV == 'Dev'){
     angular.module('app.common.services').constant('Config', {
-        // REST: 'http://localhost:8000',
-        REST: 'http://52.11.113.25',
-        DATA: 'http://54.187.171.36:8000',
-        // DATA: 'http://localhost:1337',
+        REST: 'http://54.187.171.36/api/cliente',
+        DATA: 'http://54.187.171.36/statistics',
         STATIC: 'http://localhost:4000',
-        MEDIA: 'http://192.168.0.22:8000/media'
+        // STATIC: 'https://s3-us-west-2.amazonaws.com/asociados.stage',
+        MEDIA: 'https://s3-us-west-2.amazonaws.com/asociados.stage'
     });
 }
 
 if(ENV == 'Stage'){
     angular.module('app.common.services').constant('Config', {
-        REST: 'http://54.187.171.36/api/cliente',
-        DATA: 'http://54.187.171.36/statistics',
+        REST: 'http://www.administradorapp.com/api/cliente',
+        DATA: 'http://www.administradorapp.com/statistics',
         // STATIC: 'https://s3-us-west-2.amazonaws.com/asociados-client/stage', 
-        STATIC: 'http://54.187.171.36',
-        MEDIA: 'https://s3-us-west-2.amazonaws.com/asociados.stage'
+        STATIC: 'https://s3-us-west-2.amazonaws.com/asociados-client/prod',
+        MEDIA: 'https://s3-us-west-2.amazonaws.com/staticfiles-app'
     });
 }
 
